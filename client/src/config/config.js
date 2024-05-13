f66cb32c50051b9c6603049dbe26f239c14d0196
simranlotey <simranlotey01@gmail.com>
2024-05-13 12:20:32 +0530

    React Video Call

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
-// const URL = "http://localhost:5000";
-const URL = "https://video-call-server-gm7i.onrender.com";
+const URL = "http://localhost:5000";
+// const URL = "https://video-call-server-gm7i.onrender.com";
 
 export const socket = io(URL);
 export const navbarBrand = "YourVideoShare";
